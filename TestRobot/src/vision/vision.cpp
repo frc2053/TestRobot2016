19b8a2bd44d7b26cdbb5ea765068f2b58f56b312
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2016-02-10 16:34:21 -0500

    trying to get usb cam to work

--- TestRobot/src/vision/vision.cpp
+++ TestRobot/src/vision/vision.cpp
@@ -33,84 +33,87 @@ VisionClass::VisionClass() {
  	double x,y,targetX,targetY,azimuth;
  	//int FrameCount = 0;
  	while(true) {
- 		//std::cout << "frameCount: " << FrameCount << std::endl;
- 		contours.clear();
- 		selected.clear();
- //			capture from the axis camera
- 		//printf("before read\n");
  		videoCapture.read(matOriginal);
- 		//printf("after read\n");
- //			captures from a static file for testing
- 		//matOriginal = cv::imread("/home/lvuser/original.png");
- 		//cv::imwrite("/home/lvuser/original.jpg", matOriginal);
- 		cv::resize(matOriginal, matResize, resize);
- 		matOriginal.copyTo(testingMat);
- 		cv::cvtColor(matResize,matHSV,cv::COLOR_BGR2HSV);
- 		cv::inRange(matHSV, LOWER_BOUNDS, UPPER_BOUNDS, matThresh);
- 		cv::findContours(matThresh, contours, matHeirarchy, cv::RETR_EXTERNAL,
- 				cv::CHAIN_APPROX_SIMPLE);
- 		//std::cout << "Size of contours: " << contours.size() << std::endl;
- //			make sure the contours that are detected are at least 20x20
- //			pixels with an area of 400 and an aspect ration greater then 1
- 		//printf("looping\n");
- 		for (unsigned int i = 0; i < contours.size();  i++) {
- 			cv::Rect rec = cv::boundingRect(contours[i]);
-			float aspect = (float)rec.width/(float)rec.height;
-			if(aspect > 1.0) {
-				if(rec.area() > biggestArea)
-					biggestArea = rec.area();
-					selected.push_back(contours[i]);
-				}
+ 		if(matOriginal.empty()) {
+ 			break;
  		}
- 		//std::cout << "Size of selected: " << selected.size() << std::endl;
- 		for(unsigned int i = 0; i < selected.size(); i++){
- 			cv::Rect rec = cv::boundingRect(selected[i]);
- 			//std::cout << "looping on selected!" << std::endl;
- 			cv::rectangle(matResize, rec.br(), rec.tl(), BLACK);
+ 		 		//std::cout << "frameCount: " << FrameCount << std::endl;
+ 		 		contours.clear();
+ 		 		selected.clear();
+ 		 //			capture from the axis camera
+ 		 		//printf("before read\n");
+ 		 		//printf("after read\n");
+ 		 //			captures from a static file for testing
+ 		 		//matOriginal = cv::imread("/home/lvuser/original.png");
+ 		 		//cv::imwrite("/home/lvuser/original.jpg", matOriginal);
+ 		 		cv::resize(matOriginal, matResize, resize);
+ 		 		matOriginal.copyTo(testingMat);
+ 		 		cv::cvtColor(matResize,matHSV,cv::COLOR_BGR2HSV);
+ 		 		cv::inRange(matHSV, LOWER_BOUNDS, UPPER_BOUNDS, matThresh);
+ 		 		cv::findContours(matThresh, contours, matHeirarchy, cv::RETR_EXTERNAL,
+ 		 				cv::CHAIN_APPROX_SIMPLE);
+ 		 		//std::cout << "Size of contours: " << contours.size() << std::endl;
+ 		 //			make sure the contours that are detected are at least 20x20
+ 		 //			pixels with an area of 400 and an aspect ration greater then 1
+ 		 		//printf("looping\n");
+ 		 		for (unsigned int i = 0; i < contours.size();  i++) {
+ 		 			cv::Rect rec = cv::boundingRect(contours[i]);
+ 					float aspect = (float)rec.width/(float)rec.height;
+ 					if(aspect > 1.0) {
+ 						if(rec.area() > biggestArea)
+ 							biggestArea = rec.area();
+ 							selected.push_back(contours[i]);
+ 						}
+ 		 		}
+ 		 		//std::cout << "Size of selected: " << selected.size() << std::endl;
+ 		 		for(unsigned int i = 0; i < selected.size(); i++){
+ 		 			cv::Rect rec = cv::boundingRect(selected[i]);
+ 		 			//std::cout << "looping on selected!" << std::endl;
+ 		 			cv::rectangle(matResize, rec.br(), rec.tl(), BLACK);
+ 		 		}
+ 		 //			if there is only 1 target, then we have found the target we want
+ 		 		if(selected.size() == 1){
+ 		 			//std::cout << "selected is one!" << std::endl;
+ 		 			cv::Rect rec = cv::boundingRect(selected[0]);
+ 		 //				"fun" math brought to you by miss daisy (team 341)!
+ 		 			y = rec.br().y + rec.height / 2;
+ 		 			y= -((2 * (y / matResize.cols)) - 1);
+ 		 			visionMutex.lock();
+ 		 			distance = (TOP_TARGET_HEIGHT - TOP_CAMERA_HEIGHT) /
+ 		 					tan((y * VERTICAL_FOV / 2.0 + CAMERA_ANGLE) * PI / 180);
+ 		 //				angle to target...would not rely on this
+ 		 			targetX = rec.tl().x + rec.width / 2;
+ 		 			targetX = (2 * (targetX / matResize.rows)) - 1;
+ 		 			azimuth = normalize360(targetX*HORIZONTAL_FOV /2.0 + 0);
+ 		 //				drawing info on target
+ 		 			center =  cv::Point(rec.br().x-rec.width / 2 - 15,rec.br().y - rec.height / 2);
+ 		 			cv::Point centerw =  cv::Point(rec.br().x-rec.width / 2 - 15,rec.br().y - rec.height / 2 - 20);
+ 		 			int distanceX = center.x;
+ 		 			int distanceY = center.y;
+ 		 			cv::putText(matResize, std::to_string(distanceX), center, cv::FONT_HERSHEY_PLAIN, 1, GREEN);
+ 		 			cv::putText(matResize, std::to_string(distanceY), centerw, cv::FONT_HERSHEY_PLAIN, 1, GREEN);
+ 		 			std::cout << "Center: " << center << std::endl;
+ 		 			std::cout << "Distance: " << distance << std::endl;
+ 		 			visionMutex.unlock();
+ 		 		}
+ 		 		/*if(Robot::oi->getdriverJoystick()->GetRawButton(6)) {
+ 		 			std::stringstream ss;
+ 		 			ss << "/home/lvuser/testing" << pictureTaker << ".jpg";
+ 		 			std::string s = ss.str();
+ 		 			cv::imwrite(s, matResize);
+ 		 		}*/
+ 		 		pictureTaker++;
+ 		 //			output an image for debugging
+ 		 		//cv::imwrite("/var/volatile/tmp/opencv-frame.jpg", matOriginal);
+ 		 		//image = frcCreateImage(IMAQ_IMAGE_RGB);
+ 		 		//frcReadImage(image, "/var/volatile/tmp/opencv-frame.jpg");
+ 		 		cvtColor(matResize, rgb, cv::COLOR_BGR2RGB, 0);
+ 		 		imaqArrayToImage(myImaqImage, matResize.data, matResize.cols, matResize.rows);
+ 		 		CameraServer::GetInstance()->SetImage(myImaqImage);
+ 		 		//FrameCount++;
+ 		 	//shouldRun = false;
+ 			}
  		}
- //			if there is only 1 target, then we have found the target we want
- 		if(selected.size() == 1){
- 			//std::cout << "selected is one!" << std::endl;
- 			cv::Rect rec = cv::boundingRect(selected[0]);
- //				"fun" math brought to you by miss daisy (team 341)!
- 			y = rec.br().y + rec.height / 2;
- 			y= -((2 * (y / matResize.cols)) - 1);
- 			visionMutex.lock();
- 			distance = (TOP_TARGET_HEIGHT - TOP_CAMERA_HEIGHT) /
- 					tan((y * VERTICAL_FOV / 2.0 + CAMERA_ANGLE) * PI / 180);
- //				angle to target...would not rely on this
- 			targetX = rec.tl().x + rec.width / 2;
- 			targetX = (2 * (targetX / matResize.rows)) - 1;
- 			azimuth = normalize360(targetX*HORIZONTAL_FOV /2.0 + 0);
- //				drawing info on target
- 			center =  cv::Point(rec.br().x-rec.width / 2 - 15,rec.br().y - rec.height / 2);
- 			cv::Point centerw =  cv::Point(rec.br().x-rec.width / 2 - 15,rec.br().y - rec.height / 2 - 20);
- 			int distanceX = center.x;
- 			int distanceY = center.y;
- 			cv::putText(matResize, std::to_string(distanceX), center, cv::FONT_HERSHEY_PLAIN, 1, GREEN);
- 			cv::putText(matResize, std::to_string(distanceY), centerw, cv::FONT_HERSHEY_PLAIN, 1, GREEN);
- 			std::cout << "Center: " << center << std::endl;
- 			std::cout << "Distance: " << distance << std::endl;
- 			visionMutex.unlock();
- 		}
- 		/*if(Robot::oi->getdriverJoystick()->GetRawButton(6)) {
- 			std::stringstream ss;
- 			ss << "/home/lvuser/testing" << pictureTaker << ".jpg";
- 			std::string s = ss.str();
- 			cv::imwrite(s, matResize);
- 		}*/
- 		pictureTaker++;
- //			output an image for debugging
- 		//cv::imwrite("/var/volatile/tmp/opencv-frame.jpg", matOriginal);
- 		//image = frcCreateImage(IMAQ_IMAGE_RGB);
- 		//frcReadImage(image, "/var/volatile/tmp/opencv-frame.jpg");
- 		cvtColor(matResize, rgb, cv::COLOR_BGR2RGB, 0);
- 		imaqArrayToImage(myImaqImage, matResize.data, matResize.cols, matResize.rows);
- 		CameraServer::GetInstance()->SetImage(myImaqImage);
- 		//FrameCount++;
- 	//shouldRun = false;
- 	}
-}
 
 /**
  *
@@ -128,28 +131,17 @@ void VisionClass::visionTest() {
 	matResize = cv::Mat();
 	testingMat = cv::Mat();
 	myImaqImage = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
+	videoCapture =  cv::VideoCapture();
+	videoCapture.open(0);
 //		main loop of the program
 	while(shouldRun){
-		try {
-//				opens up the camera stream and tries to load it
-			videoCapture =  cv::VideoCapture();
-//				replaces the ##.## with your team number
-			videoCapture.open("http://axis-camera.local/mjpg/video.mjpg");
-//				Example
-//				cap.open("http://10.30.19.11/mjpg/video.mjpg");
-//				wait until it is opened
-			while(!videoCapture.isOpened()){
-				printf("videoCapture could not open!\n");
-			}
-//				time to actually process the acquired images
-			processImage();
-	 		cv::imwrite("/home/lvuser/output.jpg", matOriginal);
-	 		cv::imwrite("/home/lvuser/outputrgb.jpg", rgb);
-		} catch (std::exception& e) {
-			std::cout << "MyException caught" << std::endl;
-			std::cout << e.what() << std::endl;
-			break;
+		while(!videoCapture.isOpened()){
+			printf("videoCapture could not open!\n");
 		}
+//			time to actually process the acquired images
+		processImage();
+	 	cv::imwrite("/home/lvuser/output.jpg", matOriginal);
+	 	cv::imwrite("/home/lvuser/outputrgb.jpg", rgb);
 	}
 //		make sure the java process quits when the loop finishes
 	videoCapture.release();
