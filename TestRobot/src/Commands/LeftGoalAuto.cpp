ce14ecb1a24bdf13820c13ecf826384b19be9882
Drew Williams <team2053tigertronics@gmail.com>
2016-02-24 11:44:08 -0500

    tweakauto

--- TestRobot/src/Commands/LeftGoalAuto.cpp
+++ TestRobot/src/Commands/LeftGoalAuto.cpp
@@ -6,10 +6,10 @@
 
 LeftGoalAuto::LeftGoalAuto()
 {
-	AddSequential(new DriveCommandAuto(0, 1, 0, 2, 0)); //drive in rough direction of goal
+	AddSequential(new DriveCommandAuto(0, 1, 0, .6, 0)); //drive in rough direction of goal
 	AddSequential(new DriveCommandAuto(1, 0, 0, 0.75, 45)); //slide to the left (one hop this time) a little and rotate to 45
 	AddSequential(new GoalAlignVertical(120)); //align 120 inches away from the goal
 	AddSequential(new GoalAlign()); //align horizontally with goal
 	AddSequential(new ShootHigh()); //shoot the ball
-	AddSequential(new DriveCommandAuto(0, 0, 0, 2, 180)); //turn around to go through defence
+	AddSequential(new DriveCommandAuto(0, 0, 0, 2, 180)); //turn around to go through defense
 }
