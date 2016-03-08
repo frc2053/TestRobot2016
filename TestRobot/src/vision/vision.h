801439d316082a7bdb5ffcbe6c4c38ee0f09c346
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2016-03-08 16:40:44 -0500

    vision cal 2

--- TestRobot/src/vision/vision.h
+++ TestRobot/src/vision/vision.h
@@ -35,8 +35,8 @@ private:
 	BLACK = cv::Scalar(0,0,0),
 	YELLOW = cv::Scalar(0, 255, 255),
 //	these are the threshold values in order
-	LOWER_BOUNDS = cv::Scalar(220,213,133),
-	UPPER_BOUNDS = cv::Scalar(255,255,255);
+	LOWER_BOUNDS = cv::Scalar(0,80,0),
+	UPPER_BOUNDS = cv::Scalar(30,174,103);
 	//rgb bgr
 	//lower: 218,241,227 - 227,241,218
 	//upper: 255,255,255 - 255,255,255
