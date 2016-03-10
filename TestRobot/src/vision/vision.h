1b2e6bbd00a8c7cd7ca3c77479b2d21c051029cb
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2016-03-10 10:52:18 -0500

    changed vision values

--- TestRobot/src/vision/vision.h
+++ TestRobot/src/vision/vision.h
@@ -35,8 +35,8 @@ private:
 	BLACK = cv::Scalar(0,0,0),
 	YELLOW = cv::Scalar(0, 255, 255),
 //	these are the threshold values in order
-	LOWER_BOUNDS = cv::Scalar(0,163,0),
-	UPPER_BOUNDS = cv::Scalar(83,255,40);
+	LOWER_BOUNDS = cv::Scalar(188,238,188),
+	UPPER_BOUNDS = cv::Scalar(255,255,255);
 	//rgb bgr
 	//lower: 218,241,227 - 227,241,218
 	//upper: 255,255,255 - 255,255,255
